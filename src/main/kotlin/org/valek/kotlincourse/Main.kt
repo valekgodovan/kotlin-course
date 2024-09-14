08552a9aacd661c42b049e340f2f6de02aca87fc
Valentin <valekgodovan@gmail.com>
2024-09-14 23:41:07 +0700

    add homework_3

--- src/main/kotlin/org/valek/kotlincourse/Main.kt
+++ src/main/kotlin/org/valek/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-package org.valek
+package org.valek.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.

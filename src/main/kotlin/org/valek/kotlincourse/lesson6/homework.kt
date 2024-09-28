ae78f533d8b352ecb64c295c54c5ef387fd73c2e
Valentin <valekgodovan@gmail.com>
2024-09-28 21:12:15 +0700

    fix homework6

--- src/main/kotlin/org/valek/kotlincourse/lesson6/homework.kt
+++ src/main/kotlin/org/valek/kotlincourse/lesson6/homework.kt
@@ -1,9 +1,14 @@
 package org.valek.kotlincourse.lesson6
 
+
 fun main() {
-    val month = 5
-    val season = getSeason(month)
-    println("Month $month is in $season")
+    println(getSeason(4))
+    println(dogToHumanYears(1.5))
+    println(chooseTransport(2.3))
+    println(countBonus(555.00))
+    println(recommendLook(15))
+    println(getFilmCategories(7))
+
 }
 
 fun getSeason(monthNumber: Int?): String {
@@ -13,5 +18,51 @@ fun getSeason(monthNumber: Int?): String {
         6, 7, 8 -> "Summer"
         9, 10, 11 -> "Autumn"
         else -> "Month number must be between 1 and 12"
+    }
+}
+
+fun dogToHumanYears(dogAge: Double): Double {
+    return if (dogAge <= 2) {
+        dogAge * 10.5
+    } else {
+        2 * 10.5 + (dogAge - 2) * 4
+    }
+}
+
+
+fun chooseTransport(distance: Double): String {
+    return when {
+        distance in 0.0..1.0 -> "пешком"
+        distance in 1.0..5.0 -> "велосипед"
+        else -> "автотранспорт"
+    }
+}
+
+
+fun countBonus(purchaseAmount: Double): Int {
+    return if (purchaseAmount <= 1000) {
+        (purchaseAmount / 100 * 2).toInt()
+    } else {
+        (purchaseAmount / 100 * 5).toInt()
+    }
+}
+
+//7
+fun recommendLook(temperature: Int): String {
+    return when {
+        temperature < -30 -> "Не выходите из дома"
+        temperature in -30..0 -> "Куртка и шапка"
+        temperature in 1..15 -> "Ветровка"
+        temperature in 16..35 -> "Футболка и шорты"
+        else -> "Не выходите из дома"
+    }
+}
+
+//8
+fun getFilmCategories(age: Int): String {
+    return when {
+        age < 13 -> "Доступные категории: детские"
+        age in 13..17 -> "Доступные категории: детские, подростковые"
+        else -> "Доступные категории: детские, подростковые, 18+"
     }
 }
